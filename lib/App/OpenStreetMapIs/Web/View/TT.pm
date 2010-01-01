46d3aae450f595ea8453ae11f197c7510b55461b
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-01-01 20:19:47 +0000

    Replace Catalyst welcome page with skeleton view

---
+++ lib/App/OpenStreetMapIs/Web/View/TT.pm
@@ -0,0 +1,33 @@
+package App::OpenStreetMapIs::Web::View::TT;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(TEMPLATE_EXTENSION => '.tt');
+
+=head1 NAME
+
+App::OpenStreetMapIs::Web::View::TT - TT View for App::OpenStreetMapIs::Web
+
+=head1 DESCRIPTION
+
+TT View for App::OpenStreetMapIs::Web.
+
+=head1 SEE ALSO
+
+L<App::OpenStreetMapIs::Web>
+
+=head1 AUTHOR
+
+A clever guy
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
